c00ed5fcbce1e77696669f7213e3e6fef8d09bb5
YU WANG <33480189+Sophiaaaaaa@users.noreply.github.com>
2020-04-16 20:01:08 +0800

    Update engine.py

--- protonets/engine.py
+++ protonets/engine.py
@@ -11,7 +11,9 @@ class Engine(object):
 
     def train(self, **kwargs):
         state = {
+				  # 获取模型的参数
             'model': kwargs['model'],
+            # 获取数据的参数
             'loader': kwargs['loader'],
             'optim_method': kwargs['optim_method'],
             'optim_config': kwargs['optim_config'],
@@ -21,15 +23,18 @@ class Engine(object):
             'batch': 0, # samples seen in current epoch
             'stop': False
         }
-
+			
+			# 获取model的参数的与optimizer相关的配置值（如lr等）
         state['optimizer'] = state['optim_method'](state['model'].parameters(), **state['optim_config'])
-
+			# 根据state参数，初始化学习率的衰减参数
         self.hooks['on_start'](state)
+        # 当没有达到截止条件时
         while state['epoch'] < state['max_epoch'] and not state['stop']:
+            # 训练模型
             state['model'].train()
-
+            # 用state参数重置评价指标并且进行optimizer回传
             self.hooks['on_start_epoch'](state)
-
+            # 得到每个epoch的数据的条数
             state['epoch_size'] = len(state['loader'])
 
             for sample in tqdm(state['loader'], desc="Epoch {:d} train".format(state['epoch'] + 1)):
