7bb6a5fd7df977c90d90bcac39180bba9ed66138
YU WANG <33480189+Sophiaaaaaa@users.noreply.github.com>
2020-04-18 10:56:24 +0800

    Update base.py

--- protonets/data/base.py
+++ protonets/data/base.py
@@ -17,10 +17,10 @@ class CudaTransform(object):
 class SequentialBatchSampler(object):
     def __init__(self, n_classes):
         self.n_classes = n_classes
-
+    # 获得类的总数
     def __len__(self):
         return self.n_classes
-
+    # 将类标号转化为一个longtensor
     def __iter__(self):
         for i in range(self.n_classes):
             yield torch.LongTensor([i])
@@ -33,7 +33,7 @@ class EpisodicBatchSampler(object):
 
     def __len__(self):
         return self.n_episodes
-
+    # 每个episode随机取n_way个类，返回类下标
     def __iter__(self):
         for i in range(self.n_episodes):
             yield torch.randperm(self.n_classes)[:self.n_way]
